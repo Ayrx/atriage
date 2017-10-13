6d9911f6a875a1bca719b59583bac80314d2c8ac
Terry Chia <terrycwk1994@gmail.com>
2017-10-13 16:17:06 +0800

    Add test for all_crashes property.

--- tests/test_collect.py
+++ tests/test_collect.py
@@ -11,3 +11,12 @@ def test_get_crash_statistics():
     assert res[0] == (0, "2")
     assert res[1] == (1, "+1")
     assert total == 3
+
+
+def test_all_crashes_property():
+    r = Results([
+        set(["test_case_1", "test_case_2"]),
+        set(["test_case_3"])
+    ])
+
+    assert r.all_crashes == set(["test_case_1", "test_case_2", "test_case_3"])
