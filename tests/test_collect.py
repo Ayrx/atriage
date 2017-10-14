9f896377052eb428ff0200bf16f3819441188f6c
Terry Chia <terrycwk1994@gmail.com>
2017-10-14 14:27:48 +0800

    Reduce duplication in test code.

--- tests/test_collect.py
+++ tests/test_collect.py
@@ -1,11 +1,13 @@
 from atriage.collect import Results, get_crash_statistics
 
 
+r = Results([
+    set(["test_case_1", "test_case_2"]),
+    set(["test_case_3"])
+])
+
+
 def test_get_crash_statistics():
-    r = Results([
-        set(["test_case_1", "test_case_2"]),
-        set(["test_case_3"])
-    ])
     res, total = get_crash_statistics(r)
 
     assert res[0] == (0, "2")
@@ -14,29 +16,14 @@ def test_get_crash_statistics():
 
 
 def test_all_crashes_property():
-    r = Results([
-        set(["test_case_1", "test_case_2"]),
-        set(["test_case_3"])
-    ])
-
     assert r.all_crashes == set(["test_case_1", "test_case_2", "test_case_3"])
 
 
 def test_new_crashes_property():
-    r = Results([
-        set(["test_case_1", "test_case_2"]),
-        set(["test_case_3"])
-    ])
-
     assert r.new_crashes == set(["test_case_3"])
 
 
 def test_raw_crashes_property():
-    r = Results([
-        set(["test_case_1", "test_case_2"]),
-        set(["test_case_3"])
-    ])
-
     assert r.raw_crashes == [
         set(["test_case_1", "test_case_2"]),
         set(["test_case_3"])
