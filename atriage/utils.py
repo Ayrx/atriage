cb579a2ffcd056412442adc6f23bd58f65eded47
Terry Chia <terrycwk1994@gmail.com>
2018-01-14 22:42:09 +0800

    Change `exploitable` to use `utils.run_killchild`.
    The `exploitable` command uses GDB to process the files. GDB
    spawns a child process that does not get killed on TimeoutExpired
    if the normal `subprocess.run` is used.
    
    This commit introduces a `utils.run_killchild` function that is
    exactly the same as `subprocess.run` except that the entire
    process group is killed.
    
    The `exploitable` command has been changed to use
    `utils.runkillchild` instead of `subprocess.run`.

---
+++ atriage/utils.py
@@ -0,0 +1,29 @@
+import subprocess
+import signal
+import os
+
+
+def run_killchild(*popenargs, input=None, timeout=None, check=False, **kwargs):
+    if input is not None:
+        if "stdin" in kwargs:
+            raise ValueError("stdin and input arguments may not both be used.")
+        kwargs["stdin"] = subprocess.PIPE
+
+    kwargs["preexec_fn"] = os.setsid
+    with subprocess.Popen(*popenargs, **kwargs) as process:
+        try:
+            stdout, stderr = process.communicate(input, timeout=timeout)
+        except subprocess.TimeoutExpired:
+            os.killpg(process.pid, signal.SIGKILL)
+            stdout, stderr = process.communicate()
+            raise subprocess.TimeoutExpired(process.args, timeout,
+                                            output=stdout, stderr=stderr)
+        except:
+            os.killpg(process.pid, signal.SIGINT)
+            process.wait()
+            raise
+        retcode = process.poll()
+        if check and retcode:
+            raise subprocess.CalledProcessError(retcode, process.args,
+                                                output=stdout, stderr=stderr)
+    return subprocess.CompletedProcess(process.args, retcode, stdout, stderr)
