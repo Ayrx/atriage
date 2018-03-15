22609b902768f73d06d319e8122006fd7ddf91fa
Terry Chia <terrycwk1994@gmail.com>
2018-03-15 15:24:40 +0800

    Make 1.0 release.
    This commit also updates the metadata in the setup.py file.

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="atriage",
     description="A dumb afl-fuzz triage tool.",
-    version="0.1.dev1",
+    version="0.1",
+    url="https://github.com/Ayrx/atriage",
+    author="Terry Chia",
+    author_email="terrycwk1994@gmail.com",
+    license="Apache License, Version 2.0",
+    python_requires='>=3',
     install_requires=[
         "Click",
         "tabulate",
